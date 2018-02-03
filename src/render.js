36b506ab0747cd6b36b03601aa38efef1089146e
Brandon Semilla <semibran@gmail.com>
2018-02-03 13:27:30 -0800

    add fancy render, remove autoreset

--- src/render.js
+++ src/render.js
@@ -1,4 +1,6 @@
-module.exports = function render(world, canvas) {
+module.exports = function render(state, canvas) {
+  var world = state.world
+  var cache = state.cache
   var cols = world.size[0]
   var rows = world.size[1]
   if (!canvas) canvas = document.createElement("canvas")
@@ -12,10 +14,21 @@ module.exports = function render(world, canvas) {
     for (var x = 0; x < cols; x++) {
       var index = y * cols + x
       if (world.data[index]) {
-        image.data[index * 4]     = 255
+        image.data[index * 4]     = 0
         image.data[index * 4 + 1] = 255
         image.data[index * 4 + 2] = 255
         image.data[index * 4 + 3] = 255
+      } else {
+        for (var i = 0; i < cache.length; i++) {
+          if (cache[i] && cache[i][index] === "1") {
+            var percent = (cache.length - i) / cache.length
+            image.data[index * 4]     = 0
+            image.data[index * 4 + 1] = 0
+            image.data[index * 4 + 2] = 127 * percent
+            image.data[index * 4 + 3] = 255
+            break
+          }
+        }
       }
     }
   }
--- src/script.js
+++ src/script.js
@@ -1,41 +1,43 @@
 const update = require("../lib/update")
 const render = require("./render")
 
-let distribution = 0.25
-let population = { then: 0, now: 0, streak: 0 }
-let world = {
-  time: 0,
-  rule: { birth: [ 3 ], survival: [ 2, 3 ] },
-  size: [ 256, 256 ],
-  data: new Uint8ClampedArray(256 * 256)
+let state = {
+  cache: new Array(8),
+  world: {
+    time: 0,
+    rule: parse("B3/23"),
+    size: [ 256, 256 ],
+    data: new Uint8ClampedArray(256 * 256)
+  }
 }
 
-reset(world)
-let canvas = render(world)
+reset(state.world)
+let canvas = render(state)
 document.body.appendChild(canvas)
 
 requestAnimationFrame(loop)
+
+function parse(rule) {
+  return {
+    birth: rule.slice(1, rule.indexOf("/")).split("").map(Number),
+    survival: rule.slice(rule.indexOf("S") + 1).split("").map(Number)
+  }
+}
 
 function reset(world) {
   world.time = 0
   world.data = world.data
     .fill()
-    .map(_ => Math.random() < distribution)
+    .map(_ => Math.random() < 0.5)
 }
 
 function loop() {
-  update(world)
-  population.now = 0
-  for (let i = world.data.length; i--;) population.now += world.data[i]
-  if (population.now === population.then) {
-    population.streak++
-    if (population.streak >= 120) {
-      reset(world)
-    }
-  } else {
-    population.streak = 0
+  let { cache, world } = state
+  if (cache.length) {
+    for (let i = cache.length - 1; i--;) cache[i + 1] = cache[i]
+    cache[0] = world.data.join("")
   }
-  population.then = population.now
-  render(world, canvas)
+  update(world)
+  render(state, canvas)
   requestAnimationFrame(loop)
 }
