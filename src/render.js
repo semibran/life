e456800101e56c1788c6e76e203bbe271d074291
Brandon Semilla <semibran@gmail.com>
2018-02-10 14:50:35 -0800

    panning and chemtrails

--- src/render.js
+++ src/render.js
@@ -1,62 +1,43 @@
-module.exports = function render(state, image, canvas) {
+module.exports = function render(state, image) {
   var cells = state.world
   var prev = state.prev
   var view = state.view
   var width = view.size[0]
   var height = view.size[1]
-  var context = canvas.getContext("2d")
-  canvas.width = width
-  canvas.height = height
+  var offset = [
+    Math.round(width / 2 - view.position[0]),
+    Math.round(height / 2 - view.position[1])
+  ]
 
-  var born = []
-  for (var i = 0; i < cells.length; i += 2) {
-    for (var j = 0; j < prev.length; j += 2) {
-      if (prev[j] === cells[i] && prev[j + 1] === cells[i + 1]) {
-        break
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      var index = (y * width + x) * 4
+      var value = image.data[index + 2]
+      if (value) {
+        for (var i = 0; i < cells.length; i += 2) {
+          if (cells[i] + offset[0] === x && cells[i + 1] + offset[1] === y) {
+            break
+          }
+        }
+        if (i === cells.length) {
+          image.data[index + 0] = 0x00
+          image.data[index + 1] = 0x00
+          image.data[index + 2] = value - 0xF
+          image.data[index + 3] = 0xFF
+        }
       }
-    }
-    if (j === prev.length) {
-      born.push(cells[i], cells[i + 1])
     }
   }
 
-  for (var i = 0; i < born.length; i += 2) {
-    var x = born[i + 0] + Math.round(width / 2) - view.position[0]
-    var y = born[i + 1] + Math.round(height / 2) - view.position[1]
+  for (var i = 0; i < cells.length; i += 2) {
+    var x = cells[i + 0] + offset[0]
+    var y = cells[i + 1] + offset[1]
     if (x >= 0 && x < width && y >= 0 && y < height) {
       var index = (y * width + x) * 4
-      image.data[index + 0] = 0xFF
+      image.data[index + 0] = 0x00
       image.data[index + 1] = 0xFF
       image.data[index + 2] = 0xFF
       image.data[index + 3] = 0xFF
     }
   }
-
-  var died = []
-  for (var i = 0; i < prev.length; i += 2) {
-    for (var j = 0; j < cells.length; j += 2) {
-      if (cells[j] === prev[i] && cells[j + 1] === prev[i + 1]) {
-        break
-      }
-    }
-    if (j === cells.length) {
-      died.push(prev[i], prev[i + 1])
-    }
-  }
-
-  for (var i = 0; i < died.length; i += 2) {
-    var x = died[i + 0] + Math.round(width / 2) - view.position[0]
-    var y = died[i + 1] + Math.round(height / 2) - view.position[1]
-    if (x >= 0 && x < width && y >= 0 && y < height) {
-      var index = (y * width + x) * 4
-      image.data[index + 0] = 0x00
-      image.data[index + 1] = 0x00
-      image.data[index + 2] = 0x33
-      image.data[index + 3] = 0xFF
-    }
-  }
-
-  context.putImageData(image, 0, 0)
-
-  return canvas
 }
--- src/script.js
+++ src/script.js
@@ -1,16 +1,20 @@
 const pattern = require("./pattern.json")
 const render = require("./render")
 const update = require("./update")
-const size = 256
 
+let canvas = document.createElement("canvas")
+let context = canvas.getContext("2d")
 let state = {
   time: 0,
   prev: [],
   world: pattern,
   view: {
-    size: [ size, size ],
+    canvas: canvas,
+    context: context,
+    image: null,
+    cursor: null,
     position: [ 0, 0 ],
-    cursor: null
+    size: [ window.innerWidth, window.innerHeight ]
   }
 }
 
@@ -20,23 +24,53 @@ let actions = {
     state.prev = state.world
     state.world = update(state.prev, buffer)
     state.time++
+  },
+  view: {
+    mount({ view }) {
+      document.body.appendChild(view.canvas)
+      actions.view.resize(state, window.innerWidth, window.innerHeight)
+    },
+    resize({ view }, width, height) {
+      view.size[0] = view.canvas.width = width
+      view.size[1] = view.canvas.height = height
+      view.image = view.context.createImageData(width, height)
+      actions.view.render(state)
+    },
+    render({ view }) {
+      render(state, view.image)
+      view.context.putImageData(view.image, 0, 0)
+      // view.context.drawImage(view.canvas, -view.size[0] / 2, -view.size[1] / 2, view.size[0] * 2, view.size[1] * 2)
+    }
   }
 }
 
-let canvas = document.createElement("canvas")
-canvas.width = size
-canvas.height = size
-
-let image = canvas
-  .getContext("2d")
-  .createImageData(size, size)
-
-render(state, image, canvas)
-document.body.appendChild(canvas)
+actions.view.mount(state)
 requestAnimationFrame(loop)
 
 function loop() {
   actions.update(state)
-  render(state, image, canvas)
+  actions.view.render(state)
   requestAnimationFrame(loop)
 }
+
+window.addEventListener("resize", event => {
+  actions.view.resize(state, window.innerWidth, window.innerHeight)
+})
+
+window.addEventListener("mousedown", event => {
+  state.view.cursor = [ event.pageX, event.pageY ]
+})
+
+window.addEventListener("mousemove", event => {
+  let view = state.view
+  if (view.cursor) {
+    view.position[0] -= (event.pageX - view.cursor[0]) / window.innerWidth * view.size[0]
+    view.position[1] -= (event.pageY - view.cursor[1]) / window.innerHeight * view.size[1]
+    view.cursor[0] = event.pageX
+    view.cursor[1] = event.pageY
+  }
+})
+
+window.addEventListener("mouseup", event => {
+  state.view.cursor = null
+})
